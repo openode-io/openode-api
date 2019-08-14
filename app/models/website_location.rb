ca9a1403934b62f9828f80ecb060a876ab1330aa
Martin Levesque <levesque.martin@gmail.com>
2019-08-14 00:43:05 -0400

    domain improvement

--- app/models/website_location.rb
+++ app/models/website_location.rb
@@ -4,21 +4,25 @@ class WebsiteLocation < ApplicationRecord
   belongs_to :location_server
 
   def domain
-    if self.website.domain_type == "subdomain"
-      location_subdomain = Location::SUBDOMAIN[self.location.str_id.to_sym]
+    send "domain_#{self.website.domain_type}"
+  end
 
-      first_part = ""
+  def domain_subdomain
+    location_subdomain = Location::SUBDOMAIN[self.location.str_id.to_sym]
 
-      if location_subdomain && location_subdomain != ""
-        first_part = "#{self.website.site_name}.#{location_subdomain}"
-      else
-        first_part = "#{self.website.site_name}"
-      end
+    first_part = ""
 
-      "#{first_part}.openode.io"
-    elsif self.website.domain_type == "custom_domain"
-      self.website.site_name
+    if location_subdomain && location_subdomain != ""
+      first_part = "#{self.website.site_name}.#{location_subdomain}"
+    else
+      first_part = "#{self.website.site_name}"
     end
+
+    "#{first_part}.openode.io"
+  end
+
+  def domain_custom_domain
+    self.website.site_name
   end
 
 end
