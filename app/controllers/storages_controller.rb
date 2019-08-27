722ec165149dac5ae76132651c5572beab46ae25
Martin Levesque <levesque.martin@gmail.com>
2019-08-26 21:30:21 -0400

    fix ci test

--- app/controllers/storages_controller.rb
+++ app/controllers/storages_controller.rb
@@ -9,15 +9,11 @@ class StoragesController < InstancesController
   private
 
   def prepare_storage_change
-    @gb_to_increase = storage_params["amount_gb"].to_i
+    @gb_to_increase = params["amount_gb"].to_i
 
     if @gb_to_increase <= 0
       raise ApplicationRecord::ValidationError.new("amount_gb must be positive")
     end
-  end
-
-  def storage_params
-    params.require(:storage).permit(:amount_gb)
   end
 
 end
