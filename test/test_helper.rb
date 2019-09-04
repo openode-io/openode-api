004f45d992c29d7e69220fd4713b4d3401e80b0d
Martin Levesque <levesque.martin@gmail.com>
2019-09-03 22:35:02 -0400

    coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'simplecov'
+SimpleCov.start
+
 class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
