47090b02061d4f5ac9fb37ef624554342783776f
Martin Levesque <levesque.martin@gmail.com>
2019-10-12 10:55:58 -0400

    fix tests

--- test/models/credit_action_test.rb
+++ test/models/credit_action_test.rb
@@ -16,13 +16,13 @@ class CreditActionTest < ActiveSupport::TestCase
 	test "saves properly with enough credits, with user update" do
 		website = default_website
 		credits_remaining = website.user.credits
-		ca = CreditAction.consume!(website, CreditAction::TYPE_CONSUME, 1, { with_user_update: true })
+		ca = CreditAction.consume!(website, CreditAction::TYPE_CONSUME_PLAN, 1, { with_user_update: true })
 		website.user.reload
 
 		assert_equal ca.credits_spent, 1
 		assert_equal ca.credits_remaining, credits_remaining - 1
 		assert_equal website.user.credits, credits_remaining - 1
-		assert_equal ca.action_type, CreditAction::TYPE_CONSUME
+		assert_equal ca.action_type, CreditAction::TYPE_CONSUME_PLAN
 	end
 
 	test "saves properly without enough credits, with user update" do
@@ -32,7 +32,7 @@ class CreditActionTest < ActiveSupport::TestCase
 		credits_remaining = website.user.credits
 
 		begin
-			CreditAction.consume!(website, CreditAction::TYPE_CONSUME, 1, { with_user_update: true })
+			CreditAction.consume!(website, CreditAction::TYPE_CONSUME_PLAN, 1, { with_user_update: true })
 			assert_equal true, false
 		rescue
 			website.user.reload
