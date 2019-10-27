686ba1d9a0190708ab07838b4dccb63acf2d133f
Martin Levesque <levesque.martin@gmail.com>
2019-10-26 20:46:20 -0400

    fix

--- app/models/website_bandwidth_daily_stat.rb
+++ app/models/website_bandwidth_daily_stat.rb
@@ -7,7 +7,7 @@ class WebsiteBandwidthDailyStat < History
       created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
     ).last
 
-    #puts "last stat = #{last_stat}"
+    # puts "last stat = #{last_stat}"
     unless last_stat
       WebsiteBandwidthDailyStat.create(
         ref_id: website.id,
