d9a98c511755efda56cb5dd677fdac8b71aad193
Martin Levesque <levesque.martin@gmail.com>
2019-10-27 22:44:37 -0400

    wip

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,18 @@ class Order < ApplicationRecord
   serialize :content, JSON
 
   belongs_to :user
+
+  validates :user, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :payment_status, presence: true
+  validates :content, presence: true
+
+  before_create :apply_coupon
+  after_create :send_confirmation_email
+
+  def apply_coupon
+    # if any
+  end
+
+  def send_confirmation_email; end
 end
