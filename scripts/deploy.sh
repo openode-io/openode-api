be62209b7a415ddc39030b286f9fe8c3edf24aaa
Martin Levesque <levesque.martin@gmail.com>
2019-11-16 12:31:29 -0500

    delayed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,5 @@ sshpass -p $API_PASSWORD ssh -o StrictHostKeyChecking=no $API_USER@$API_HOST \
   echo 'bundle install' && ./bin/bundle install && \
   RAILS_ENV=$RAILS_ENV ./bin/rails runner 'puts ENV[\"RAILS_ENV\"]' && \
   RAILS_ENV=$RAILS_ENV ./bin/rails db:migrate && \
-  pm2 reload openode_jobs && \
+  RAILS_ENV=production ./bin/delayed_job --pid-dir=tmp/pids -n 5 restart && \
   pm2 list" # replace with reload
