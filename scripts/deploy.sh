08b84c1791d32a77c583495c7afb6356df423c85
Martin Levesque <levesque.martin@gmail.com>
2019-11-16 10:30:36 -0500

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,4 +11,4 @@ sshpass -p $API_PASSWORD ssh -o StrictHostKeyChecking=no $API_USER@$API_HOST \
   echo '333' && \
   ./bin/bundle install && \
   echo '444' && \
-  ./bin/rails runner 'puts ENV[\"RAILS_ENV\"]'"
+  RAILS_ENV=production ./bin/rails runner 'puts ENV[\"RAILS_ENV\"]'"
