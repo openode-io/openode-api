f6ca160972ce2cab2b08c661f4c5b42ec7b7841b
Martin Levesque <levesque.martin@gmail.com>
2019-11-16 09:45:48 -0500

    dep test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 
 sshpass -p $API_PASSWORD ssh -o StrictHostKeyChecking=no $API_USER@$API_HOST \
-  "cd $PROJECT_PATH && git pull && ./bin/bundle install && rails runner 'puts ENV[\"RAILS_ENV\"]'"
+  "export PATH && cd $PROJECT_PATH && git pull && ./bin/bundle install && rails runner 'puts ENV[\"RAILS_ENV\"]'"
