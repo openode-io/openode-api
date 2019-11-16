d2907f88e4427b72b9b2f5f2097c662453bd7142
Martin Levesque <levesque.martin@gmail.com>
2019-11-16 10:37:39 -0500

    test dep

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,8 @@
 
 sshpass -p $API_PASSWORD ssh -o StrictHostKeyChecking=no $API_USER@$API_HOST \
-  "PATH=\"$PATH:/usr/local/rvm/rubies/ruby-2.6.3/bin/\" && \
-  echo '111' && \
-  echo $PATH && \
+  "PATH=\"$PATH:$RUBY_BIN_PATH\" && \
   cd $PROJECT_PATH && \
-  pwd && \
-  ls -la /root && \
-  echo '222' && \
-  git pull && \
-  echo '333' && \
-  ./bin/bundle install && \
-  echo '444' && \
-  RAILS_ENV=production ./bin/rails runner 'puts ENV[\"RAILS_ENV\"]'"
+  echo 'CWD:' && pwd && \
+  echo 'git pulling' && git pull && \
+  echo 'bundle install' && ./bin/bundle install && \
+  RAILS_ENV=$RAILS_ENV ./bin/rails runner 'puts ENV[\"RAILS_ENV\"]'"
