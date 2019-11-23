127263c7fca1ecac38b7b2a874c086bee47236e1
Martin Levesque <levesque.martin@gmail.com>
2019-11-22 21:59:07 -0500

    begin permissions

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -1,4 +1,12 @@
 class Collaborator < ApplicationRecord
   belongs_to :website
   belongs_to :user
+
+  PERMISSION_DEPLOY = 'deploy'
+  PERMISSION_DNS = 'dns'
+  PERMISSION_ALIAS = 'alias'
+  PERMISSION_STORAGE_AREA = 'storage_area'
+  PERMISSION_LOCATION = 'location'
+  PERMISSION_PLAN = 'plan'
+  PERMISSION_CONFIG = 'config'
 end
