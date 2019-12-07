cf0861c896e9232a40adb268fc29c0c1d811b4fe
Martin Levesque <levesque.martin@gmail.com>
2019-12-07 10:58:34 -0500

    api openode

---
+++ lib/api/openode.rb
@@ -0,0 +1,26 @@
+require 'rest-client'
+
+module Api
+  class Openode
+    attr_reader :token
+
+    def initialize(args = {})
+      @token = args[:token]
+    end
+
+    def execute(method, path = "/", args = {})
+      headers = {
+        "x-auth-token": @token,
+        "params": args[:params] || {}
+      }
+
+      url = "#{Rails.configuration.API_URL}#{path}"
+
+      JSON.parse(RestClient::Request.execute(method: method,
+                                             url: url,
+                                             timeout: 10,
+                                             payload: args[:payload],
+                                             headers: headers))
+    end
+  end
+end
