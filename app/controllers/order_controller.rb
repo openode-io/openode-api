2e7d8853059ed6b0647efeb7765727b74f42cfe1
Martin Levesque <levesque.martin@gmail.com>
2020-02-04 20:22:24 -0500

    verify paypal orders twoway

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,5 +1,11 @@
 class OrderController < ApplicationController
   def paypal
+    filtered_params = filtered_order_params(params)
+
+    unless Payment::Paypal.transaction_valid?(filtered_params)
+      return json(result: 'error', msg: 'Order invalid')
+    end
+
     parsed_order = Payment::Paypal.parse(params)
 
     unless Payment::Paypal.completed?(parsed_order)
@@ -17,5 +23,20 @@ class OrderController < ApplicationController
   rescue StandardError => e
     Ex::Logger.error(e)
     json(result: 'error', msg: e.to_s)
+  end
+
+  private
+
+  def filtered_order_params(params)
+    all_params = params.to_unsafe_h
+    keys = all_params.keys
+
+    result = {}
+
+    keys.each do |key|
+      result[key] = all_params[key] unless %w[action controller order].include?(key)
+    end
+
+    result
   end
 end
