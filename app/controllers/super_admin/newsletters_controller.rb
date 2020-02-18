5c0307af929aa247eb9e4d57a209004b8cf2c127
Martin Levesque <levesque.martin@gmail.com>
2020-02-18 14:14:47 -0500

    create newsletter

--- app/controllers/super_admin/newsletters_controller.rb
+++ app/controllers/super_admin/newsletters_controller.rb
@@ -4,4 +4,16 @@ class SuperAdmin::NewslettersController < SuperAdmin::SuperAdminController
 
     json(default_listing(Newsletter, attributes_to_search))
   end
+
+  def create
+    custom_recipients = params['newsletter']['custom_recipients']
+    json(Newsletter.create!(newsletter_params.merge(custom_recipients: custom_recipients)))
+  end
+
+  protected
+
+  def newsletter_params
+    params.require(:newsletter)
+          .permit(:title, :recipients_type, :content, :custom_recipients)
+  end
 end
--- test/controllers/super_admin/newsletters_controller_test.rb
+++ test/controllers/super_admin/newsletters_controller_test.rb
@@ -12,4 +12,29 @@ class SuperAdmin::NewslettersControllerTest < ActionDispatch::IntegrationTest
     assert_equal response.parsed_body.length, 1
     assert_equal response.parsed_body[0]["title"], "Hello newsletter"
   end
+
+  test "create with valid params" do
+    n_to_create = {
+      title: 'hellonn',
+      recipients_type: 'custom',
+      content: 'bla bla <html></html>',
+      custom_recipients: ['test1@gmaill.com', 'test2@gmaill.com']
+    }
+
+    post '/super_admin/newsletters',
+         as: :json,
+         params: {
+           newsletter: n_to_create
+         },
+         headers: super_admin_headers_auth
+
+    assert_response :success
+
+    n = Newsletter.find_by! id: response.parsed_body['id']
+
+    assert_equal n.title, n_to_create[:title]
+    assert_equal n.recipients_type, n_to_create[:recipients_type]
+    assert_includes n.content, n_to_create[:content]
+    assert_equal n.custom_recipients, n_to_create[:custom_recipients]
+  end
 end
