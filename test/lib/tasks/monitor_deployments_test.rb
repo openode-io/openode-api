9274d1b90ae47398eb90a3f0cdf3e729194e734e
Martin Levesque <levesque.martin@gmail.com>
2020-04-06 20:05:40 -0400

    monitor pod status

--- test/lib/tasks/monitor_deployments_test.rb
+++ test/lib/tasks/monitor_deployments_test.rb
@@ -36,6 +36,36 @@ class LibMonitorDeploymentsTest < ActiveSupport::TestCase
     end
   end
 
+  test "monitor deployments - with one online without status" do
+    Website.all.each do |w|
+      w.status = Website::STATUS_OFFLINE
+      w.save!
+    end
+
+    WebsiteStatus.all.each(&:destroy)
+
+    website = default_website
+    website.status = Website::STATUS_ONLINE
+    website.type = Website::TYPE_KUBERNETES
+    website.save!
+
+    cmd = get_kubernetes_method(website).kubectl(
+      website_location: website.website_locations.first,
+      with_namespace: true,
+      s_arguments: "get pods -o json"
+    )
+
+    prepare_ssh_session(cmd, IO.read('test/fixtures/kubernetes/empty_pod.json'))
+
+    assert_scripted do
+      begin_ssh
+      invoke_task "monitor_deployments:pod_status"
+
+      assert_equal website.statuses.length, 1
+      assert_equal website.statuses.first.obj, nil
+    end
+  end
+
   test "monitor deployments - without any online" do
     Website.all.each do |w|
       w.status = Website::STATUS_OFFLINE
