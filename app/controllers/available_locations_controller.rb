ce8ac02e8b67a6ba3c85831d0616cc94669f8767
Martin Levesque <levesque.martin@gmail.com>
2020-05-06 20:05:07 -0400

    API Provide list of IPs for each location + documentation #270

---
+++ app/controllers/available_locations_controller.rb
@@ -0,0 +1,52 @@
+require 'droplet_kit'
+
+class AvailableLocationsController < ApplicationController
+  api!
+  def index
+    type = params['type'] || 'kubernetes'
+
+    manager = if type
+                # TODO: -> deprecate next line
+                real_type = type == 'docker' ? 'internal' : type
+                real_type = 'kubernetes' unless %w[internal kubernetes].include?(real_type)
+                CloudProvider::Manager.instance.first_of_type(real_type)
+              else
+                CloudProvider::Manager.instance
+              end
+
+    json(manager.available_locations)
+  end
+
+  api :GET, 'global/available-locations/:str_id/ips'
+  description 'Retrieve the server IPs at a given location.'
+  param :str_id, String, desc: 'Location str id, from /global/available-locations', required: true
+  def ips
+    assert ENV['DO_ACCESS_TOKEN']
+    cache_key = "/api/global/available-locations/"
+
+    result = Rails.cache.fetch("#{cache_key}#{params['str_id']}/ips", expires_in: 10.minutes) do
+      client = DropletKit::Client.new(access_token: ENV['DO_ACCESS_TOKEN'])
+      clusters = client.kubernetes_clusters.all
+
+      looking_for_cluster = "k8s-#{params['str_id']}"
+      cluster = clusters.find { |c| c.name == looking_for_cluster }
+      return json([]) unless cluster
+
+      all_droplets = client.droplets.all
+      pools = client.kubernetes_clusters.node_pools(id: cluster.id)
+
+      pools.map do |pool|
+        pool.nodes.map do |node|
+          droplet = all_droplets.find { |d| d.name == node.name }
+          public_network = droplet&.networks&.v4&.find { |n| n.type == 'public' }
+
+          public_network ? public_network.ip_address : nil
+        end
+            .select(&:present?)
+      end
+           .flatten
+    end
+
+    json(result)
+  end
+end
--- test/controllers/global_controller_test.rb
+++ test/controllers/global_controller_test.rb
@@ -34,53 +34,6 @@ class GlobalControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test '/global/available-locations' do
-    get '/global/available-locations', as: :json
-
-    assert_response :success
-
-    canada = response.parsed_body.find { |l| l['str_id'] == 'canada' }
-    assert_equal canada['str_id'], 'canada'
-    assert_equal canada['full_name'], 'Montreal (Canada2)'
-    assert_equal canada['country_fullname'], 'Canada2'
-
-    usa = response.parsed_body.find { |l| l['str_id'] == 'usa' }
-    assert_equal usa['str_id'], 'usa'
-    assert_equal usa['full_name'], 'New York (USA)'
-    assert_equal usa['country_fullname'], 'United States'
-  end
-
-  test '/global/available-locations type internal' do
-    get '/global/available-locations?type=internal', as: :json
-
-    assert_response :success
-
-    assert_equal response.parsed_body.length, 1
-    assert_equal response.parsed_body[0]['str_id'], 'canada2'
-  end
-
-  # TODO: deprecate
-  test '/global/available-locations type docker' do
-    get '/global/available-locations?type=docker', as: :json
-
-    assert_response :success
-
-    assert_equal response.parsed_body.length, 1
-    assert_equal response.parsed_body[0]['str_id'], 'canada2'
-  end
-
-  test '/global/available-locations with invalid type should default to kubernetes' do
-    get '/global/available-locations?type=undefined', as: :json
-
-    assert_response :success
-
-    canada = response.parsed_body.find { |l| l['str_id'] == 'canada' }
-    assert_equal canada['str_id'], 'canada'
-
-    usa = response.parsed_body.find { |l| l['str_id'] == 'usa' }
-    assert_equal usa['str_id'], 'usa'
-  end
-
   test '/global/available-plans' do
     get '/global/available-plans', as: :json
 
