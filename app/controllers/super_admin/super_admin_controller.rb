9bf43bd10a9f562beb652c0f4cf182794b3613c1
Martin Levesque <levesque.martin@gmail.com>
2020-07-10 22:11:12 -0400

    allow listing generic list in super admin

--- app/controllers/super_admin/super_admin_controller.rb
+++ app/controllers/super_admin/super_admin_controller.rb
@@ -2,6 +2,21 @@ class SuperAdmin::SuperAdminController < ApplicationController
   before_action :authorize
   before_action :requires_admin_level
 
+  def generic_index
+    attribs = params['attributes'] || ''
+    attributes_to_search = attribs.split(',')
+
+    entity_method = params['entity_method']
+
+    entity = params['entity'].constantize
+
+    if entity_method.present?
+      entity = entity.send(entity_method)
+    end
+
+    json(default_listing(entity, attributes_to_search))
+  end
+
   protected
 
   def requires_admin_level
