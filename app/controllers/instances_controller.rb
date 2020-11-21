e515f548225aab66d786652cff4a4d1fb6078f57
Martin Levesque <levesque.martin@gmail.com>
2020-11-21 11:49:40 -0500

    add repo url summary

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -74,6 +74,7 @@ class InstancesController < ApplicationController
     w_obj["last_deployment_id"] = w.deployments.last&.id
     w_obj["ip"] = w.first_ip
     w_obj["active"] = w.active?
+    w_obj["repository_url"] = w.secret&.dig(:repository_url)
     w_obj["out_of_memory_detected"] = w.recent_out_of_memory_detected?
 
     extra_storage = w.website_locations&.first&.extra_storage || 0
