2bcc0df4668bf68bb92c4b5fe6aba4c9326af4b3
Martin Levesque <levesque.martin@gmail.com>
2021-03-04 03:16:57 +0000

    change support

--- app/controllers/super_admin/support_controller.rb
+++ app/controllers/super_admin/support_controller.rb
@@ -6,18 +6,27 @@ Freshdesk.password_or_x = "X"
 
 class SuperAdmin::SupportController < SuperAdmin::SuperAdminController
   def contact
-    message = params['message']&.gsub(/\n/, '<br>')
-    attributes = params.except('message')
+    #message = params['message']&.gsub(/\n/, '<br>')
+    #attributes = params.except('message')
 
-    params_freshdesk = {
-      status: 2,
-      priority: 1,
-      description: "#{message}<br /><br /><hr />#{attributes.inspect}",
-      subject: params['title'] || "opeNode Contact",
-      cc_emails: [],
-      email: params['email']
-    }
-    Freshdesk::Ticket.create_a_ticket(params: params_freshdesk)
+    #params_freshdesk = {
+    #  status: 2,
+    #  priority: 1,
+    #  description: "#{message}<br /><br /><hr />#{attributes.inspect}",
+    #  subject: params['title'] || "opeNode Contact",
+    #  cc_emails: [],
+    #  email: params['email']
+    #}
+    #Freshdesk::Ticket.create_a_ticket(params: params_freshdesk)
+    SupportMailer.with(
+      attributes: params,
+      email_to: ENV['DEFAULT_EMAIL']
+    ).contact.deliver_now
+
+    SupportMailer.with(
+      attributes: params,
+      email_to: "info@openode.io"
+    ).contact.deliver_now
 
     json({})
   end
