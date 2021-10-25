a4f5d6f75241b5649fd725491a31e4e0f8eb8ece
Martin Levesque <levesque.martin@gmail.com>
2021-10-25 10:47:10 -0400

    wip

--- lib/deployment_method/gcloud_run.rb
+++ lib/deployment_method/gcloud_run.rb
@@ -24,14 +24,46 @@ module DeploymentMethod
       super(options)
     end
 
+    def gcloud_cmd(options = {})
+      website, website_location = get_website_fields(options)
+      project_path = website.repo_dir
+      "timeout 300 sh -c \"cd #{project_path} && gcloud --project #{GCLOUD_PROJECT_ID} #{options[:subcommand]}\""
+    end
+
+    def image_tag_url(options = {})
+      website, website_location = get_website_fields(options)
+
+      tag_name = DeploymentMethod::Util::InstanceImageManager.tag_name(
+        website: website,
+        execution_id: runner.execution.id
+      )
+
+      "gcr.io/#{GCLOUD_PROJECT_ID}/#{website.site_name}:#{tag_name}"
+    end
+
     def build_image(options = {})
       website, website_location = get_website_fields(options)
 
-      puts "GCLOUD_PROJECT_ID = #{GCLOUD_PROJECT_ID}"
+      image_url = image_tag_url(options)
+
+      result = ex("gcloud_cmd", {
+        website: website,
+        website_location: website_location,
+        subcommand: "builds submit --tag #{image_url}"
+      })
+      puts "image_url built = #{image_url}"
+
+      image_url
     end
 
     def launch(options = {})
       website, website_location = get_website_fields(options)
+
+      #ex("gcloud_cmd", {
+      #  website: website,
+      #  website_location: website_location,
+      #  subcommand: "auth activate-service-account --key-file=/home/martin/works/openode-api/config/service_accounts/openode-deploy.json"
+      #})
 
       build_image(options)
     end
