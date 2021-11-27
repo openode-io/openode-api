c63fd1e7193a4b0db4cfc9dee2955ad9a08e17e2
Martin Lévesque <levesque.martin@gmail.com>
2021-11-27 18:45:49 -0500

    One month free trial (#636)
    * fix 200 mb plan
    
    * one month free trial

--- test/controllers/account_controller_test.rb
+++ test/controllers/account_controller_test.rb
@@ -245,7 +245,7 @@ class AccountControllerTest < ActionDispatch::IntegrationTest
     assert_equal user.token, response.parsed_body['token']
     assert_equal user.newsletter, 1
     assert_equal user.credits.positive?, true
-    assert_in_delta user.credits, 4.83870
+    assert_in_delta user.credits, 185.806
 
     mail_sent = ActionMailer::Base.deliveries.first
     assert_equal mail_sent.subject, 'Welcome to opeNode!'
