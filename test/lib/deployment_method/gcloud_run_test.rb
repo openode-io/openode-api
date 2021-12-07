cdb919ebe0c440415e78948003337a6c4d1297bc
Martin Lévesque <levesque.martin@gmail.com>
2021-12-07 10:04:09 -0500

    Unit test gcloud deployment (#649)
    * wip
    
    * wip
    
    * launch test
    
    * fix test
    
    * fix lint
    
    * fix

--- test/lib/deployment_method/gcloud_run_test.rb
+++ test/lib/deployment_method/gcloud_run_test.rb
@@ -1,5 +1,6 @@
 
 require 'test_helper'
+require 'test_gcloud_run_helper'
 
 class DeploymentMethodGcloudRunTest < ActiveSupport::TestCase
   def setup
@@ -25,5 +26,110 @@ class DeploymentMethodGcloudRunTest < ActiveSupport::TestCase
     result = gcloud_run_method.env_variables(@website)
 
     assert_equal result, ""
+  end
+
+  # gcloud_cmd
+
+  test 'gcloud_cmd - happy path' do
+    result = gcloud_run_method.gcloud_cmd(
+      website: @website, website_location: @website_location
+    )
+
+    assert_equal result, "timeout 400 sh -c 'cd #{@website.repo_dir} " \
+      "&& gcloud --project openode '"
+  end
+
+  test 'gcloud_cmd - with custom timeout' do
+    result = gcloud_run_method.gcloud_cmd(
+      website: @website,
+      website_location: @website_location,
+      timeout: 10
+    )
+
+    assert_equal result, "timeout 10 sh -c 'cd #{@website.repo_dir} " \
+      "&& gcloud --project openode '"
+  end
+
+  # image_tag_url
+
+  test 'image_tag_url - happy path' do
+    result = gcloud_run_method.image_tag_url(
+      website: @website,
+      website_location: @website_location
+    )
+
+    site_name = @website.site_name
+    expected_url = "gcr.io/openode/#{site_name}:#{site_name}--#{@website.id}--" \
+      "#{gcloud_run_method.runner.execution.id}"
+    assert_equal result, expected_url
+  end
+
+  # build_image
+
+  test 'build_image - happy path' do
+    run_method = gcloud_run_method
+    run_method.ex_return = [
+      {
+
+      },
+      {
+        stderr: "Created [https://cloudbuild.googleapis.com/build-id-1234567]",
+        exit_code: 0
+      },
+      {
+        exit_code: 0,
+        stdout: "output"
+      }
+    ]
+    result = run_method.build_image(
+      website: @website,
+      website_location: @website_location
+    )
+
+    site_name = @website.site_name
+    exec_id = @runner.execution.id
+
+    expected_result = "gcr.io/openode/#{site_name}:#{site_name}--#{@website.id}--#{exec_id}"
+    assert_equal result, expected_result
+  end
+
+  # launch
+
+  test 'launch - happy path' do
+    run_method = gcloud_run_method
+    run_method.ex_return = [
+      {
+        exit_code: 0
+      },
+      {
+        stderr: "Created [https://cloudbuild.googleapis.com/build-id-1234567]",
+        exit_code: 0
+      },
+      {
+        exit_code: 0,
+        stdout: "output"
+      },
+      {
+        exit_code: 0
+      },
+      {
+        stdout: "[{\"status\": {\"url\": \"https://serviceurl\"}}]",
+        exit_code: 0
+      }
+    ]
+    run_method.ex_stdout_return = [
+      "output_logs"
+    ]
+
+    result = run_method.launch(
+      website: @website,
+      website_location: @website_location
+    )
+
+    assert_equal result, true
+    assert_equal run_method.ex_history.count, 5
+    assert_equal run_method.ex_stdout_history.count, 1
+
+    assert_equal @website_location.obj["gcloud_url"], "https://serviceurl"
   end
 end
