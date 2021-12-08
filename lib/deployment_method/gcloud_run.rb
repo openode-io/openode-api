cd3f5a59ca2f7f75bbd808fe365136b3642983fc
Martin Lévesque <levesque.martin@gmail.com>
2021-12-08 11:30:30 -0500

    fix tests (#651)

--- lib/deployment_method/gcloud_run.rb
+++ lib/deployment_method/gcloud_run.rb
@@ -467,4 +467,26 @@ module DeploymentMethod
       notify('info', "\n\n*** Final Deployment state: #{runner&.execution&.status&.upcase} ***\n")
     end
   end
+
+  class GcloudRunTest < GcloudRun
+    attr_accessor :ex_return, :ex_history, :ex_stdout_return, :ex_stdout_history
+
+    def ex(cmd, options = {})
+      @ex_history ||= []
+      @ex_history << { cmd: cmd, options: options }
+      @ind_ex_return ||= -1
+      @ind_ex_return += 1
+
+      @ex_return[@ind_ex_return]
+    end
+
+    def ex_stdout(cmd, options_cmd = {}, _global_options = {})
+      @ex_stdout_history ||= []
+      @ex_stdout_history << { cmd: cmd, options: options_cmd }
+      @ind_ex_stdout_return ||= -1
+      @ind_ex_stdout_return += 1
+
+      @ex_stdout_return[@ind_ex_stdout_return]
+    end
+  end
 end
